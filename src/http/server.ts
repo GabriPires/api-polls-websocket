fd1fedb3907a415b87c15ba263bec33c05f8d8dd
GabriPires <gabpiresdev@gmail.com>
2024-02-07 09:19:49 -0300

    feat: create poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,26 @@
 import fastify from 'fastify'
+import z from 'zod'
+
+import { prisma } from '../lib/prisma'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return { message: 'Hello World' }
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  })
+
+  const { title } = createPollBody.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  })
+
+  return reply.status(201).send({
+    pollId: poll.id,
+  })
 })
 
 app
