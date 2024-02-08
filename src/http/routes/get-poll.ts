d4d2bfec0248816f71a7feb891f3fd24037b5a15
GabriPires <gabpiresdev@gmail.com>
2024-02-08 08:13:52 -0300

    feat: use redis to rank poll votes

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import z from 'zod'
 
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -25,8 +26,36 @@ export async function getPoll(app: FastifyInstance) {
       },
     })
 
+    if (!poll) {
+      return reply.status(400).send({ message: 'Poll not found' })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce(
+      (acc, line, index) => {
+        if (index % 2 === 0) {
+          const value = result[index + 1]
+          Object.assign(acc, {
+            [line]: Number(value),
+          })
+        }
+
+        return acc
+      },
+      {} as Record<string, number>,
+    )
+
     return reply.status(200).send({
-      poll,
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => ({
+          id: option.id,
+          title: option.title,
+          votes: option.id in votes ? votes[option.id] : 0,
+        })),
+      },
     })
   })
 }
